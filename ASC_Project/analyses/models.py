003e5aaa6af4530f213408afa7aefe9a4e2968f8
Shayan Fahimi <shayanfa@mail.ubc.ca>
2019-08-09 17:11:26 -0700

    showing properties in each page and adding a submit page

--- ASC_Project/analyses/models.py
+++ ASC_Project/analyses/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from .choice import TYPE_OF_ANALYSIS
+from .choice import TYPE_OF_BC
+
+
 class Analysis(models.Model):
     name = models.CharField(max_length=30, unique=True, help_text='Name of analysis')
     description = models.CharField(max_length=200, help_text='Description')
@@ -16,21 +20,30 @@ class Mesh(models.Model):
     def __str__(self):
         return self.name
 
-class Material(models.Model):
-    name = models.CharField(max_length=30, unique=True, help_text='Name of material')
-    typ = models.PositiveSmallIntegerField(default=0, help_text='Type of material, 0 for default')
-    viscosity = models.FloatField(default=0, help_text='Enter a number for viscosity')
-    permeability = models.FloatField(default=0, help_text='Enter a number for permeability')
-    analysis = models.ForeignKey(Analysis, related_name='material', on_delete=models.CASCADE)
+class Resin(models.Model):
+    name = models.CharField(max_length=30, unique=True, help_text='Name of Resin')
+    viscosity = models.FloatField(default=0.02, help_text='Enter a number for viscosity')
+    analysis = models.OneToOneField(Analysis, related_name='resin', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
+class Preform(models.Model):
+    name = models.CharField(max_length=30, unique=True, help_text='Name of Preform')
+    thickness = models.FloatField(default=0.01, help_text='Thickness')
+    K11 = models.FloatField(default=1e-10, help_text='Enter a number for K11')
+    K12 = models.FloatField(default=0, help_text='Enter a number for K12')
+    K22 = models.FloatField(default=2e-10, help_text='Enter a number for K22')
+    analysis = models.ForeignKey(Analysis, related_name='preform', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Section(models.Model):
     name = models.CharField(max_length=30, unique=True)
-#   region={}
-
-    material = models.ForeignKey(Material, related_name='material', on_delete=models.SET_NULL, null=True)
+    preform = models.ForeignKey(Preform, related_name='preform', on_delete=models.SET_NULL, null=True)
+    rotate = models.FloatField(default=0, help_text='Degree of rotation')
     analysis = models.ForeignKey(Analysis, related_name='section', on_delete=models.CASCADE)
 
     def __str__(self):
@@ -38,7 +51,9 @@ class Section(models.Model):
 
 class Step(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    analysis = models.ForeignKey(Analysis, related_name='step', on_delete=models.CASCADE)
+    typ = models.IntegerField(choices=TYPE_OF_ANALYSIS, default=0)
+    endtime = models.FloatField(default=0, help_text='Time to end analysis')
+    analysis = models.OneToOneField(Analysis, related_name='step', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -46,8 +61,9 @@ class Step(models.Model):
 
 class BC(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    typ = models.PositiveSmallIntegerField(default=0)
-    region = {}
+    typ = models.IntegerField(choices=TYPE_OF_BC, default=0)
+    value = models.FloatField(default=0, help_text='Value on Boundary Condition')
+    analysis = models.ForeignKey(Analysis, related_name='bc', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
